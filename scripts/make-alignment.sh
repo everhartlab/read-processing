22a40c623b6c3c522b350767895b45837f857771
Zhian Kamvar <zkamvar@unl.edu>
2017-10-09 15:23:52 -0500

    add unpaired reads to alignment

--- scripts/make-alignment.sh
+++ scripts/make-alignment.sh
@@ -43,10 +43,24 @@ module load $BT2MOD
 CMD="bowtie2 -x $IDX -S $DIR/"
 
 # Create named pipes because bowtie2 is stupid and can't handle gzipped files >:(
-MKFIFO="mkfifo \$TMPDIR/M1.fifo \$TMPDIR/M2.fifo"
+# Note that I have to make four of these, two for the paired reads and two for the
+# unpaired reads.
+MKFIFO="mkfifo \
+\$TMPDIR/M1.fifo \
+\$TMPDIR/M2.fifo \
+\$TMPDIR/U1.fifo \
+\$TMPDIR/U2.fifo"
+
 M1=$SAMPLE"_1$SUF"
 M2=$SAMPLE"_2$SUF"
-WFIFO="zcat $M1 > \$TMPDIR/M1.fifo & zcat $M2 > \$TMPDIR/M2.fifo &"
+U1=$SAMPLE"_1U.fq.gz"
+U2=$SAMPLE"_2U.fq.gz"
+
+WFIFO="zcat $M1 > \$TMPDIR/M1.fifo \
+& zcat $M2 > \$TMPDIR/M2.fifo \
+& zcat $U1 > \$TMPDIR/U1.fifo \
+& zcat $U2 > \$TMPDIR/U2.fifo \
+&"
 
 # Info Line: 
 #          0	     1	          2	      3	      4	5	   6	       7	         8
@@ -54,18 +68,23 @@ WFIFO="zcat $M1 > \$TMPDIR/M1.fifo & zcat $M2 > \$TMPDIR/M2.fifo &"
 INFO=($(zcat $M1 | head -n 1 | sed 's/:/ /g'))
 
 # Setting arguments
-ARGS="-1 \$TMPDIR/M1.fifo -2 \$TMPDIR/M2.fifo --maxins 800 --fr"
+ARGS="-1 \$TMPDIR/M1.fifo,\$TMPDIR/U1.fifo \
+-2 \$TMPDIR/M2.fifo,\$TMPDIR/U2.fifo \
+--maxins 800 \
+--fr"
 BASE=$(basename $SAMPLE)
 
 # Setup read group information for GATK
 RGID="--rg-id ${INFO[2]}.${INFO[3]}"
-RG="--rg SM:$BASE --rg PL:ILLUMINA --rg LB:RUN.${INFO[1]}"
+RG="--rg SM:$BASE \
+--rg PL:ILLUMINA \
+--rg LB:RUN.${INFO[1]}"
 
 # Run the command through time with memory and such reporting.
 # warning: there is an old bug in GNU time that overreports memory usage
 # by 4x; this is compensated for in the SGE_Plotdir script.
 TIME='/usr/bin/env time -f " \\tFull Command:                      %C \\n\\tMemory (kb):                       %M \\n\\t# SWAP  (freq):                    %W \\n\\t# Waits (freq):                    %w \\n\\tCPU (percent):                     %P \\n\\tTime (seconds):                    %e \\n\\tTime (hh:mm:ss.ms):                %E \\n\\tSystem CPU Time (seconds):         %S \\n\\tUser   CPU Time (seconds):         %U " '
-CMD="$MKFIFO; $WFIFO $CMD$BASE.sam $ARGS $RGID $RG"
+CMD="$MKFIFO; $WFIFO $TIME $CMD$BASE.sam $ARGS $RGID $RG"
 
 
 # Write details to stdout
